42d9618728e25d79f7085a00930712a3bfdc509b
Gilberetzu <euri.herasme@gmail.com>
2021-10-12 14:04:08 -0400

    Added planet generator link to landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,3 +4,7 @@
 <div>
     Hello there! Sorry there is nothing to see here yet! (^_^')
 </div>
+
+<div>
+    Here you can access a <a href="/PlanetGenerator">Planet Generator</a> I hope you like it!!
+</div>
